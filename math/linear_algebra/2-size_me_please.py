d6aba74ca1da8a5e570f9e45c9befd482f5e61cc
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-07-30 22:30:45 -0400

    calculating shape of matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    """
+    Function that calculates the shape of a matrix
+    """
+    shape = []
+    while type(matrix) == list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
