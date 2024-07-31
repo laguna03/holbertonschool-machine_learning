58f40b9d3cea62dbbc5d0e4855579044605fe80e
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-07-30 23:07:13 -0400

    returning transponse of 2d matrix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 """
-Module for matrix_transpose
+Module for matrix_transpose.
 """
+
 
 def matrix_transpose(matrix):
     """
-    Function that returns the transpose of a 2D matrix
-    """
+    Function that returns the transpose of a 2D matrix.
 
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    Args:
+        matrix (list of lists): The matrix to transpose.
+
+    Returns:
+        list of lists: The transposed matrix.
+    """
+    return [[matrix[j][i] for j in range(len(matrix))]
+            for i in range(len(matrix[0]))]
