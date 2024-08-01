e422ed2f69ff1f630dde21e93a7e707419ba9fe1
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-08-01 15:07:07 -0400

    concat two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module for 6-howdy_partner.py.
+Contains a function that concatenates two arrays.
+"""
+
+def cat_arrays(arr1, arr2):
+    """
+    Function that concatenates two arrays.
+
+    Args:
+        arr1 (list): The first array.
+        arr2 (list): The second array.
+
+    Returns:
+        list: A new list containing the elements of arr1 followed by the
+              elements of arr2.
+    """
+    return arr1 + arr2
