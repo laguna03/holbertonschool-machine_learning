062411f28230cf14d7047b3edae67db16c3b58f2
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-08-04 18:40:26 -0400

    Matrix multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+Module 8-ridin_bareback
+Provides a function mat_mul to perform matrix multiplication.
+"""
+
 def mat_mul(mat1, mat2):
+    """
+    Performs matrix multiplication of two 2D lists (matrices).
+
+    Args:
+        mat1 (list of list of int/float): The first matrix.
+        mat2 (list of list of int/float): The second matrix.
+
+    Returns:
+        list of list of int/float or None: The result of the matrix multiplication,
+        or None if the matrices cannot be multiplied due to incompatible dimensions.
+    """
     if len(mat1[0]) != len(mat2):
         return None
     result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
