4003d37d4280e4f65e23abfa57f1f48d9a470714
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-08-04 19:32:06 -0400

    matrix mult

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Module for np_matmul
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Function that performs matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
