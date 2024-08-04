5c36bfd455a518bd316c8edd9a777ed8daed778d
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-08-04 18:57:22 -0400

    My scale

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+"""
+Module 10-ill_use_my_scale
+Provides a function np_shape to get the shape of a numpy matrix.
+"""
+
 def np_shape(matrix):
+    """
+    Returns the shape of a numpy matrix.
+
+    Args:
+        matrix (numpy.ndarray): The matrix for which to find the shape.
+
+    Returns:
+        tuple: A tuple representing the dimensions of the matrix.
+    """
     return matrix.shape
