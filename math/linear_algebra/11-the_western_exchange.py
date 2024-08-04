a976acafc703b25c0af1eb010cfeeabf0a29901d
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-08-04 19:03:43 -0400

    Transponse

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Module 11-the_western_exchange
+Provides a function np_transpose to transpose a numpy matrix.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a numpy matrix.
+
+    Args:
+        matrix (numpy.ndarray): The matrix to transpose.
+
+    Returns:
+        numpy.ndarray: The transposed matrix.
+    """
+    return matrix.transpose()
