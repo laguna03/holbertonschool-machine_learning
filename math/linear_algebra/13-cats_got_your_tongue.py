c5629cfec698026aae4ae3d695180e8cbb7b9fb7
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-08-04 19:19:45 -0400

    concat along spec axis

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,22 @@
+#!/user/bin/env python3
+import numpy as np
+
+"""
+Module cats_got_your_tongue
+Provides a function np_cat to concatenate numpy matrices.
+"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    Concatenates numpy matrices along a specified axis.
+
+    Args:
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
+        axis (int): The axis along which to concatenate the matrices.
+
+    Returns:
+        numpy.ndarray: The concatenated matrix.
+    """
+    return np.concatenate((mat1, mat2), axis=axis)
