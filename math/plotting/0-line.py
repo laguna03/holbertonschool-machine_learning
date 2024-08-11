13b36e756f48bbcc6a303cfde70d9362402646ba
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-08-10 23:08:59 -0400

    first task

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -9,6 +9,13 @@ import matplotlib.pyplot as plt
 
 
 def line():
+    """
+    Plots a line graph of the cubic values of integers from 0 to 10.
+
+    The function uses Matplotlib to create a plot where the y-values
+    are the cubes of integers from 0 to 10, displayed in red.
+    The x-axis is limited to the range from 0 to 10.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
