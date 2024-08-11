410ca6b6787c39e2c58d257b2ba6e827330a7469
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-08-10 23:08:05 -0400

    first task

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module contains a function to plot a simple line graph using Matplotlib.
+The graph plots the cubic values of integers from 0 to 10.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
