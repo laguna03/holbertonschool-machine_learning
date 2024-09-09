e7da70e6446ca8a8210d62fd3de10ba287166d56
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-09-08 22:35:57 -0400

    Completing tasks

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Script for accuracy in tensor flow"""
+
+import tensorflow.compat.v1 as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    method to calculate the accuracy of a prediction in a DNN
+    Args:
+        y: input data type label in a placeholder
+        y_pred: type tensor that contains the DNN prediction
+
+    Returns: Prediction accuracy
+
+    """
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    return accuracy
