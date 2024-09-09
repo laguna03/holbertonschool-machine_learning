3895493a2cc7437003c08b79181cd3a15c23f8a6
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-09-08 22:38:15 -0400

    Completing tasks

---
+++ supervised_learning/tensorflow/4-calculate_accuracy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Script for loss in tensorflow"""
+
+import tensorflow.compat.v1 as tf
+
+
+def calculate_loss(y, y_pred):
+    """
+    Method to calculate the cross-entropy loss
+    of a prediction
+    Args:
+        y: input data type label in a placeholder
+        y_pred: type tensor that contains the DNN prediction
+
+    Returns:
+
+    """
+    loss = tf.losses.softmax_cross_entropy(y, y_pred)
+    return loss
---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Script to train a model in tensorflow
+   using gradient descent optimizer
+"""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    method to train a model in a DNN with tf.
+    Args:
+        loss: cross-entropy loss function
+        alpha: learning rate
+
+    Returns: Trained operation of de DNN using gradient descent
+
+    """
+    opt = tf.train.GradientDescentOptimizer(alpha).minimize(loss)
+    return opt
