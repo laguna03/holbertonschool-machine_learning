f5ef86bb6a2a1be3b67ab3a37230c41befebe24a
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-09-08 22:40:55 -0400

    Completing tasks

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Script that evaluates a DNN in tf"""
+
+import tensorflow.compat.v1 as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    Method to evaluate a DNN
+    Args:
+        X: input data
+        Y: labels from X
+        save_path: Location of trained model
+
+    Returns: Evaluated model
+
+    """
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph("{}.meta".format(save_path))
+        saver.restore(sess, save_path)
+        #  tf.get_collection() returns a list. Retrieve only the 1st one
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+
+        eval_y_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        eval_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        eval_loss = sess.run(loss, feed_dict={x: X, y: Y})
+        return eval_y_pred, eval_accuracy, eval_loss
