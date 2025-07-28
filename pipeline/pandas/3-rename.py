017c5a3b5d6304200962b8b517b38a3c4994726d
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2025-07-27 23:02:41 -0400

    tasks

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
-"""This module loads data from a file into a dataframe"""
+"""This module renames a column of a dataframe"""
 import pandas as pd
 
 
-def from_file(filename, delimiter):
-    """This function loads data from a file into a dataframe
+def rename(df):
+    """This function renames the column Timestamp to Datetime,
+    converts the timestamp values to datetime values,
+    and displays only the Datetime and Close columns.
+
     Args:
-        filename: the file to load from
-        delimiter: the column separator
-        Returns: the loaded dataframe
+        df: the dataframe to modify
+
+    Returns:
+        The modified dataframe
     """
-    return pd.read_csv(filename, delimiter=delimiter)
+    # Rename the column 'Timestamp' to 'Datetime'
+    df = df.rename(columns={'Timestamp': 'Datetime'})
+
+    # Convert the 'Datetime' column to datetime objects
+    df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
+
+    # Display only the 'Datetime' and 'Close' columns
+    df = df[['Datetime', 'Close']]
+
+    return df
